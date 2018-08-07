e267602e6c1b22149c3ed8379243a2bdb4e04184
张斌 <zhangbin@gsafety.com>
2018-08-07 20:34:30 +0800

    修改--完善消息管理，添加消息、消息集对象

--- src/Zxc.Python/zxcPy.All.Base/zxcPy.Base/myManager_Msg.py
+++ src/Zxc.Python/zxcPy.All.Base/zxcPy.Base/myManager_Msg.py
@@ -6,9 +6,48 @@ Created on  张斌 2018-07-23 11:00:00
 
     消息处理器--通用消息处理器
 """
-import sys, os, copy
-import myWeb_urlLib, myVoice
+import sys, os, copy, datetime
+import myEnum, myWeb_urlLib #, myVoice 
 
+
+#定义消息类型枚举
+myMsgType = myEnum.enum('TEXT', 'IMAGE', 'VOICE', 'VIDEO')
+
+#自定义消息对象
+class myMsg():
+    def __init__(self, msg, msgID, msgType = myMsgType.TEXT): 
+        self.msg = msg              #消息内容
+        self.msgID = msgID          #消息标识
+        self.msgType = msgType      #消息类型
+        self.msgUrl = ""            #消息链接(多媒体)
+        self.msgTime = datetime.datetime.now()  #消息示例时间 
+#自定义消息对象集
+class myMsgs():
+    def __init__(self, usrName, usrNameNick, userID): 
+        self.usrName = usrName          #归属用户
+        self.usrNameNick = usrNameNick  #归属用户
+        self.userID = userID            #归属用户ID
+        self.usrID_sys = userID         #归属用户ID_sys
+        self.usrMsgs = []               #消息集
+
+    #添加消息
+    def Add(self, msg, msgID = '', msgType = myMsgType.TEXT):
+        if(msg == ""): return False
+        pMsg = myMsg(msg, msgID, msgType)
+        return self._Add(pMsg)
+    def _Add(self, msg):
+        if(msg == None): return False
+        self.usrMsgs.append(msg)
+        return True
+    
+    #查找消息
+    def Find(self, msgID):
+        nLen = len(self.usrMsgs)
+        for ind in range(nLen - 1, -1, -1):
+            pMsg = self.usrMsgs[ind]
+            if(pMsg.msgID == msgID):
+                return pMsg
+        return None
 
 #消息处理器--通用消息处理器
 class myManager_Msg():
@@ -16,7 +55,10 @@ class myManager_Msg():
         self.usePrint = True
         self.useVoice = False
         self.useLineMsg = True
-        self.msgExramp = {"user":'', "text":'', "type":'TEXT', "plat":'weixin'} #消息样例
+        self.msgExramp = {"usrID":'', "usrName":'', "usrNameNick":'', "msg":'', "msgID":'', "msgType":'TEXT', "plat":'weixin'} #消息样例
+        self.msgLogs = {}
+        self.msgInd_Name = {}
+        self.msgInd_Nick = {}
 
         self.usrWebs = {}           #在线消息集       
         if(self.useLineMsg):
@@ -25,16 +67,46 @@ class myManager_Msg():
                 pWeb = myWeb_urlLib.myWeb(msgUrls[x])
                 self.usrWebs[x] = pWeb
         
+    #添加消息
+    def Log(self, usrID, usrName, usrNameNick, msg, msgID, msgType = myMsgType.TEXT):
+        if(usrID == ""): return False
+        pMsgs = self._Find_Log(usrID, usrName, usrNameNick, True)
+        if(pMsgs != None):
+            if(msg == ""): return False
+            pMsg = myMsg(msg, msgID, msgType)
+            pMsgs._Add(pMsg) 
+        return True
+    def _Log(self, msg = {}):
+        usrID = msg.get('usrID', "")
+        if(usrID == ""): return False
+        usrName = msg.get('usrName', "")
+        usrNameNick = msg.get('usrNameNick', "")
+        return self.Log(usrID, usrName, usrNameNick, msg.get('msg', ""), msg.get('msgID', ""), msg.get('msgType', ""))
+    def _Find_Log(self, usrID, usrName = "", usrNameNick = "", bCreatAuto = True):
+        pMsgs = self.msgLogs.get(usrID, None)
+        if(pMsgs == None):
+            pMsgs = self.msgInd_Name.get(usrName, None)
+            if(pMsgs == None):
+                pMsgs = self.msgInd_Nick.get(usrNameNick, None)
+
+        #创建新消息集
+        if(pMsgs == None and bCreatAuto):
+            pMsgs = myMsgs(usrName, usrNameNick, usrID)
+            self.msgLogs[usrID] = pMsgs
+            if(usrName != ""): self.msgInd_Name[usrName] = usrID
+            if(usrNameNick != ""): self.msgInd_Nick[usrNameNick] = usrID
+        return pMsgs
+
     #消息处理
     def OnHandleMsg(self, msg):
-        strMsg = msg["text"]
+        strMsg = msg.get('msg')
 
         #文字输出
         if(self.usePrint): print(strMsg)
 
         #声音输出
-        if(self.useVoice):
-            myVoice.Say_thrd(msg["text"])
+        #if(self.useVoice):
+        #    myVoice.Say_thrd(msg["text"])
 
         #在线消息输出
         typePlatform = msg.get("plat", "weixin")
@@ -43,7 +115,7 @@ class myManager_Msg():
             if(pWeb != None):   
                 #微信输出
                 if(typePlatform == "weixin"):    
-                    wxPath = "weixin/" + msg["user"] + "/" + strMsg + "/" + msg["type"]
+                    wxPath = "weixin/" + msg["usrID"] + "/" + strMsg + "/" + msg["msgType"]
                     pWeb.Do_API_get(wxPath, "微信API-py")
                 else:
                     pass
@@ -62,11 +134,19 @@ if __name__ == '__main__':
 
    #组装消息 
    msg = {}
-   msg["user"] = "茶叶一主号"
-   msg["text"] = "测试消息py"
-   msg["type"] = "TEXT"
+   msg["usrName"] = "茶叶一主号"
+   msg["usrID"] = "zxcID"
+   msg["msg"] = "测试消息py"
+   msg["msgID"] = "msgID-***"
+   msg["msgType"] = "TEXT"
    msg["plat"] = "weixin"
    pMMsg.OnHandleMsg(msg)
+
+
+   #消息日志
+   pMMsg.Log("zxcID", "茶叶一主号", "", "测试消息py-00", "")
+   pMMsg._Log(msg)
+   print(pMMsg._Find_Log("zxcID").Find(msg["msgID"]))
 
    print()
     
