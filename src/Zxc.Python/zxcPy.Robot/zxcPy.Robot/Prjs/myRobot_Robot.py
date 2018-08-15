2dd129cfc48c68e25f2373d2c819f7fa07817c8b
zhangbin <zhangbin@gsafety.com>
2018-08-15 17:23:07 +0800

    修改--完善机器人类，增加图灵机器人接口实现，v2参数报错，放弃

--- src/Zxc.Python/zxcPy.Robot/zxcPy.Robot/Prjs/myRobot_Robot.py
+++ src/Zxc.Python/zxcPy.Robot/zxcPy.Robot/Prjs/myRobot_Robot.py
@@ -6,11 +6,11 @@ Created on  张斌 2017-11-17 15:16:00
 
     Weixin网页版消息处理接口（功能库）--机器人类--聊天机器人
 """
-import sys, os, time ,mySystem
+import sys, ast, os, time ,mySystem
 
 #引用根目录类文件夹--必须，否则非本地目录起动时无法找到自定义类 
 mySystem.Append_Us("", False) 
-import myRobot
+import myRobot, myData_Json, myDebug, myWeb_urlLib
 from myGlobal import gol   
 
     
@@ -21,22 +21,59 @@ class myRobot_Robot(myRobot.myRobot):
         self.doTitle = "聊天机器人"     #说明 
         self.prjName = "聊天机器人"     #功能名
         self.doCmd = "@@ChatRobot"      #启动命令 
-        
+
+        #创建机器人api  
+        self.apiRobot = myWeb_urlLib.myWeb("http://openapi.tuling123.com", "", False)    #图灵机器人
+        self.data = myData_Json.Json_Object()
+        self.data = {
+	                    "reqType":0,
+                        "perception": {
+                            "inputText": {
+                                "text": "附近的酒店"
+                            }
+                        },
+                        "userInfo": {
+                            "apiKey": "de79dec1dc6b41f59ed3e4c743b1f089",
+                            "userId": "zxcRobot"
+                        }
+                    }
+        self.data = {
+                        "key": "de79dec1dc6b41f59ed3e4c743b1f089",
+	                    "info": "",
+	                    "loc": "",
+	                    "userid": "zxcRobot"
+                    }
+
+
     #消息处理接口
     def _Done(self, Text, msgID = "", isGroup = False, idGroup = "", usrID = "", usrName = ""):
         #聊天机器人(接入第三方接口进行处理)
-        strText = Text + "--by zxcChatRobot"
+        strText = self._Done_ByTuling(Text)
         return strText 
 
     def _Title_User_Opened(self): 
         return "消息机器人自动回复..."
-        
+    
+    #图灵机器人
+    def _Done_ByTuling(self, Text):
+        #组装post数据
+        #self.data["perception"]["inputText"] = Text
+        self.data["info"] = Text
+
+        #请求数据
+        data = self.apiRobot.Do_Post("openapi/api", self.data, "myRobot_Robot")
+        body = data.decode(encoding = "utf-8")
+        msg = ast.literal_eval(body) 
+        return msg['text']
+         
 
 #主启动程序
 if __name__ == "__main__":
     pR = myRobot_Robot("zxc", "zxcID");
     pp = pR.Done("@@ChatRobot")
-    print(pR.Done("Hello"))
+    myDebug.Debug(pR.Done("Hello"))
+    myDebug.Debug(pR.Done("北京 天气"))
+    myDebug.Debug(pR.Done("光山 天气"))
     pR.Done("@@ChatRobot")
     print()
 
