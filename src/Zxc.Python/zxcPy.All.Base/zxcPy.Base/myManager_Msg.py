29f71f242066fb367c21bb33fbd036dfb5b03add
zhangbin <zhangbin@gsafety.com>
2018-08-20 02:51:59 +0800

    修改--调整消息管理消息记录，增加dict方法，消息写入文件等；

--- src/Zxc.Python/zxcPy.All.Base/zxcPy.Base/myManager_Msg.py
+++ src/Zxc.Python/zxcPy.All.Base/zxcPy.Base/myManager_Msg.py
@@ -7,7 +7,7 @@ Created on  张斌 2018-07-23 11:00:00
     消息处理器--通用消息处理器
 """
 import sys, os, copy, datetime
-import myEnum, myData, myData_Trans, myDebug, myWeb_urlLib, myMQ_Rabbit #, myVoice 
+import myEnum, myData, myData_Trans, myDebug, myIO, myWeb_urlLib, myMQ_Rabbit #, myVoice 
 
 
 #定义消息类型枚举
@@ -16,29 +16,46 @@ myMsgPlat = myEnum.enum('robot', 'wx')
 
 #自定义消息对象
 class myMsg():
-    def __init__(self, msg, msgID, msgType = myMsgType.TEXT): 
+    def __init__(self, msg, msgID, usrFrom, msgType = myMsgType.TEXT): 
         self.msg = msg              #消息内容
         self.msgID = msgID          #消息标识
         self.msgType = msgType      #消息类型
+        self.usrFrom = usrFrom      #消息来源
         self.msgUrl = ""            #消息链接(多媒体)
         self.msgTime = datetime.datetime.now()  #消息示例时间 
+    def toCSVString(self):
+        strCSV = "\n" + myData_Trans.Tran_ToDatetime_str(self.msgTime)
+        strCSV += "," + self.usrFrom
+        strCSV += "," + self.msg
+        strCSV += "," + self.msgType
+        return strCSV
 #自定义消息对象集
 class myMsgs():
-    def __init__(self, usrName, usrNameNick, userID): 
+    def __init__(self, userID, usrName, usrNameNick, dirLog = ""): 
         self.usrName = usrName          #归属用户
-        self.usrNameNick = usrNameNick  #归属用户
+        self.usrNameNick = usrNameNick  #归属用户昵称
         self.userID = userID            #归属用户ID
         self.usrID_sys = userID         #归属用户ID_sys
         self.usrMsgs = []               #消息集
+        self.pathLog = ""
+        if(dirLog != ""):
+            self.pathLog = dirLog + self.usrNameNick + ".csv"
+            if(os.path.exists(self.pathLog) == False):
+                myIO.Save_File(self.pathLog, "时间,用户名,消息,消息类型", False, True) 
 
     #添加消息
-    def Add(self, msg, msgID = '', msgType = myMsgType.TEXT):
+    def Add(self, msg, msgID = '', usrFrom = '', msgType = myMsgType.TEXT):
         if(msg == ""): return False
         pMsg = myMsg(msg, msgID, msgType)
         return self._Add(pMsg)
-    def _Add(self, msg):
+    def _Add(self, msg, bSave = False):
         if(msg == None): return False
         self.usrMsgs.append(msg)
+
+        #文件追加数据内容
+        if(self.pathLog != ""):
+            with open(self.pathLog, 'a+') as f:
+                f.write(msg.toCSVString())   
         return True
     
     #查找消息
@@ -52,7 +69,7 @@ class myMsgs():
 
 #消息处理器--通用消息处理器(转发需要主动初始平台对应消息队列及API)
 class myManager_Msg():
-    def __init__(self):
+    def __init__(self, dir = ""):
         self.usePrint = True
         self.useVoice = False  
         self.msgExramp = {"usrID":'', "usrName":'', "usrNameNick":'', "msg":'', "msgID":'', "msgType":'TEXT', "groupID":'', "plat":''} #消息样例
@@ -61,6 +78,7 @@ class myManager_Msg():
         self.msgInd_Nick = {}
         self.usrWebs = {}           #在线消息集    
         self.usrMQs = {}            #消息队列  
+        self.dirLog = dir
     #初始API、消息队列    
     def _Init(self, plat = myMsgPlat.wx, msgUrl_API = "http://127.0.0.1:8666/zxcAPI/weixin", msgMQ_Sender = myMQ_Rabbit.myMQ_Rabbit(True, 'zxcMQ_wx')):
         if(plat == None or plat == ""): return 
@@ -74,32 +92,49 @@ class myManager_Msg():
         if(msgUrl_API != ""):                 
             pWeb = myWeb_urlLib.myWeb(msgUrl_API, "", False)                #按消息分类标识初始对应web对象并存入dict
             self.usrWebs[plat] = pWeb
+    #初始消息日志路径   
+    def Init_LogDir(self, dir):
+        self.dirLog = dir
+        myIO.mkdir(self.dirLog)
         
     #添加消息
-    def Log(self, usrID, usrName, usrNameNick, msg, msgID, msgType = myMsgType.TEXT):
-        if(usrID == ""): return False
-        pMsgs = self._Find_Log(usrID, usrName, usrNameNick, True)
+    def Log(self, usrID, usrName, usrNameNick, msg, msgID, msgType = myMsgType.TEXT, idGroup = '', nameGroup = '', nameSelf = ''):
+        usrInfo = self.OnCreatMsg_UsrInfo(usrID, usrName, usrNameNick, idGroup, nameGroup, nameSelf)
+        return self.Log_ByDict(msg, msgID, msgType, usrInfo)
+    def Log_ByDict(self, msg, msgID, msgType, usrInfo):
+        pMsgs = self._Find_Log_ByDict(usrInfo, True)
         if(pMsgs != None):
             if(msg == ""): return False
-            pMsg = myMsg(msg, msgID, msgType)
+            nameSelf = usrInfo.get('usrNameSelf', "")
+            usrName = myData.iif(nameSelf == "", usrInfo.get('usrName', ""), nameSelf)
+            pMsg = myMsg(msg, msgID, usrName, msgType)
             pMsgs._Add(pMsg) 
         return True
     def _Log(self, msg = {}):
-        usrID = msg.get('usrID', "")
-        if(usrID == ""): return False
-        usrName = msg.get('usrName', "")
-        usrNameNick = msg.get('usrNameNick', "")
-        return self.Log(usrID, usrName, usrNameNick, msg.get('msg', ""), msg.get('msgID', ""), msg.get('msgType', ""))
-    def _Find_Log(self, usrID, usrName = "", usrNameNick = "", bCreatAuto = True):
-        pMsgs = self.msgLogs.get(usrID, None)
+        return self.Log_ByDict(msg.get('msg', ""), msg.get('msgID', ""), msg.get('msgType', ""), msg)
+    def _Find_Log_ByDict(self, usrInfo, bCreatAuto = True):
+        pMsgs = None
+        nameGroup = usrInfo.get('groupName', "")
+        if(nameGroup != ""):
+            usrID = usrInfo.get('groupID', "")
+            usrName = nameGroup
+            usrNameNick = ""
+        else:
+            usrID = usrInfo.get('usrID', "")
+            usrName = usrInfo.get('usrName', "")
+            usrNameNick = usrInfo.get('usrNameNick', "")
+        return self._Find_Log(usrID, usrName, usrNameNick, bCreatAuto)
+    def _Find_Log(self, usrID, usrName, usrNameNick, bCreatAuto = False):
+        #依次查询
+        if(usrID != ""): pMsgs = self.msgLogs.get(usrID, None)
         if(pMsgs == None):
-            pMsgs = self.msgInd_Name.get(usrName, None)
+            if(usrName != ""): pMsgs = self.msgInd_Name.get(usrName, None)
             if(pMsgs == None):
-                pMsgs = self.msgInd_Nick.get(usrNameNick, None)
+                if(usrNameNick != ""):pMsgs = self.msgInd_Nick.get(usrNameNick, None)
 
         #创建新消息集
         if(pMsgs == None and bCreatAuto):
-            pMsgs = myMsgs(usrName, usrNameNick, usrID)
+            pMsgs = myMsgs(usrID, usrName, usrNameNick, self.dirLog)
             self.msgLogs[usrID] = pMsgs
             if(usrName != ""): self.msgInd_Name[usrName] = usrID
             if(usrNameNick != ""): self.msgInd_Nick[usrNameNick] = usrID
@@ -137,6 +172,16 @@ class myManager_Msg():
         if(bCreatTime):
             msg['time'] = myData_Trans.Tran_ToTime_int()
         return msg
+    #创建封装用户信息
+    def OnCreatMsg_UsrInfo(self, usrID, usrName, nameNick, groupID, groupName, nameSelf):
+        usrInfo = {}
+        usrInfo['usrID'] = usrID
+        usrInfo['usrName'] = usrName
+        usrInfo['usrNameNick'] = nameNick
+        usrInfo['groupID'] = groupID
+        usrInfo['groupName'] = groupName
+        usrInfo['usrNameSelf'] = nameSelf      #自己发自己标识 
+        return usrInfo
 
     #消息超时校检
     def Check_TimeOut(self, msg, nTimeOut = 600, nTimeNow = -1):  
@@ -158,6 +203,7 @@ gol._Get_Setting('manageMsgs', None)._Init(plat = myMsgPlat.wx, msgMQ_Sender = m
 
 if __name__ == '__main__':
    pMMsg = gol._Get_Setting('manageMsgs')
+   pMMsg.Init_LogDir("D:/myGit/zxcProj/src/Zxc.Python/zxcPy.Robot/Log/Msgs/")
 
    #组装消息 
    msg = pMMsg.OnCreatMsg()
@@ -174,7 +220,7 @@ if __name__ == '__main__':
    #消息日志
    pMMsg.Log("zxcID", "茶叶一主号", "", "测试消息py-00", "")
    pMMsg._Log(msg)
-   print(pMMsg._Find_Log("zxcID").Find(msg["msgID"]))
+   print(pMMsg._Find_Log("zxcID",  "茶叶一主号", "").Find(msg["msgID"]))
 
    print()
     
