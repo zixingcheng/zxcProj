85edc5c60268a60ee502db394d55b693bf099048
zhangbin <zhangbin@gsafety.com>
2019-01-22 10:18:41 +0800

    修改--添加基于盛派的微信公众号

---
+++ src/Zxc.Core/zxcCore.Weixin/zxcCore.Weixin.MP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace zxcCore.Weixin.MP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                //如果不配置下面这条信息，会导致无法直接访问//当然不用下面这个可以用Nginx来配置
+                .UseUrls("http://*:80")
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
