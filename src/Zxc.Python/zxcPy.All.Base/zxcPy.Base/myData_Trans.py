45dc2aa1beb29cb6b065a7d260e8eb99add79af8
zhangbin <zhangbin@gsafety.com>
2019-03-30 19:12:46 +0800

    修改--新增 汉字首字母转换函数；

--- src/Zxc.Python/zxcPy.All.Base/zxcPy.Base/myData_Trans.py
+++ src/Zxc.Python/zxcPy.All.Base/zxcPy.Base/myData_Trans.py
@@ -184,6 +184,79 @@ def Tran_ToStr(lstV = [], symbol = ','):
         strV += symbol + str(x)
     if(len(lstV) > 0): strV = strV[1:]
     return strV
+#转换汉子首字母
+def Tran_ToStr_FirstLetters(str_input, bUpper = False):
+    if isinstance(str_input, str):
+        unicode_str = str_input
+    else:
+        try:
+            unicode_str = str_input.decode('utf8')
+        except:
+            try:
+                unicode_str = str_input.decode('gbk')
+            except:
+                print ('unknown coding')
+                return
+      
+    pStr = ''
+    for one_unicode in unicode_str:
+        pStr = pStr + str(Tran_ToStr_FirstLetter(one_unicode))
+    if(bUpper): return pStr.upper()
+    return pStr
+def Tran_ToStr_FirstLetter(unicodeWord):
+    str1 = unicodeWord.encode('gbk')
+    try:        
+        ord(str1)
+        return str1
+    except:
+        asc = str1[0] * 256 + str1[1] - 65536
+        if asc >= -20319 and asc <= -20284:
+            return 'a'
+        if asc >= -20283 and asc <= -19776:
+            return 'b'
+        if asc >= -19775 and asc <= -19219:
+            return 'c'
+        if asc >= -19218 and asc <= -18711:
+            return 'd'
+        if asc >= -18710 and asc <= -18527:
+            return 'e'
+        if asc >= -18526 and asc <= -18240:
+            return 'f'
+        if asc >= -18239 and asc <= -17923:
+            return 'g'
+        if asc >= -17922 and asc <= -17418:
+            return 'h'
+        if asc >= -17417 and asc <= -16475:
+            return 'j'
+        if asc >= -16474 and asc <= -16213:
+            return 'k'
+        if asc >= -16212 and asc <= -15641:
+            return 'l'
+        if asc >= -15640 and asc <= -15166:
+            return 'm'
+        if asc >= -15165 and asc <= -14923:
+            return 'n'
+        if asc >= -14922 and asc <= -14915:
+            return 'o'
+        if asc >= -14914 and asc <= -14631:
+            return 'p'
+        if asc >= -14630 and asc <= -14150:
+            return 'q'
+        if asc >= -14149 and asc <= -14091:
+            return 'r'
+        if asc >= -14090 and asc <= -13119:
+            return 's'
+        if asc >= -13118 and asc <= -12839:
+            return 't'
+        if asc >= -12838 and asc <= -12557:
+            return 'w'
+        if asc >= -12556 and asc <= -11848:
+            return 'x'
+        if asc >= -11847 and asc <= -11056:
+            return 'y'
+        if asc >= -11055 and asc <= -10247:
+            return 'z'
+        return ''
     
 #字符串变为转义字符（"转为\"）
 def To_Escape_str(strData, bOneLine = False):
@@ -201,7 +274,7 @@ def To_Escape_str(strData, bOneLine = False):
         strValue = strValue.replace(" : ",":")
           
     return strValue    
-    
+
 
 
 if __name__ == '__main__':   
@@ -228,6 +301,8 @@ if __name__ == '__main__':
     print(Tran_ToDatetime_str())
     print()
 
+    #转换汉子首字母
+    print(Tran_ToStr_FirstLetters(u'广联达', True))
 
     #转换成时间数组
     dt = "2016-05-05 20:28:54"
