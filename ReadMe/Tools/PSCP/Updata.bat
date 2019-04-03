2d168338c19bcb6026a4b8478fc2b1f45dd32aa2
zhangbin <zhangbin@gsafety.com>
2019-04-03 16:05:36 +0800

    修改--添加Linux交互脚本到git管理；

---
+++ ReadMe/Tools/PSCP/Updata.bat
@@ -0,0 +1,35 @@
+:: 设置路径信息
+set varPath_py=C:\Python35-32\Lib\site-packages
+set varPath_base=D:\myGit\zxcProj
+set varPath=%varPath_base%\ReadMe\Tools\PSCP\UpLoad
+set varPath_Url=root@39.105.196.175:
+set varPath_Linux=%varPath_Url%/root/Public
+set varPath_Prj=%varPath_Linux%/myPrjs/zxcProj
+
+
+:: 上传文件到Linux目录
+echo zxcvbnm123*|pscp -r %varPath%\Temp %varPath_Linux%/UpLoad
+
+:: 基础库-python
+echo zxcvbnm123*|pscp -r %varPath_py%\myPy_Libs %varPath_Url%/opt/python3.5/lib/python3.5/site-packages
+
+
+
+:: 下载Linux文件
+echo zxcvbnm123*|pscp -r %varPath_Linux%/UpLoad/Updata %varPath%
+
+:: 下载备份Linux配置文件
+echo zxcvbnm123*|pscp -r %varPath_Prj%/src/Zxc.Python/zxcPy.Quote/Setting %varPath%/Setting/Quote
+echo zxcvbnm123*|pscp -r %varPath_Prj%/src/Zxc.Python/zxcPy.Robot/Setting %varPath%/Setting/Robot
+
+echo zxcvbnm123*|pscp -r %varPath_Prj%/src/Zxc.Python/zxcPy.Robot/Data/DB_Bill %varPath%\Setting\Robot\Data 
+echo zxcvbnm123*|pscp -r %varPath_Prj%/src/Zxc.Python/zxcPy.Weixin/Data/Pic/QR.png %varPath%\Setting\Weixin\Data
+
+
+
+:: 上传配置文件到Linux目录
+echo zxcvbnm123*|pscp -r %varPath_base%\src\Zxc.Python\zxcPy.Quote\Setting %varPath_Prj%/src/Zxc.Python/zxcPy.Quote
+echo zxcvbnm123*|pscp -r %varPath_base%\src\Zxc.Python\zxcPy.Robot\Setting %varPath_Prj%/src/Zxc.Python/zxcPy.Robot
+
+echo zxcvbnm123*|pscp -r %varPath_base%\src\Zxc.Python\zxcPy.Robot\Data\DB_Bill %varPath_Prj%/src/Zxc.Python/zxcPy.Robot/Data/DB_Bill/
+
