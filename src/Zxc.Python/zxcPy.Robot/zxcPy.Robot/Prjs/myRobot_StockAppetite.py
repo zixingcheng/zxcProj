fb4157a516da1843fb0da03558c9273280606097
墨紫 <164750211@qq.com>
2019-09-13 15:23:48 +0800

    修改--优化股票偏好图片类型判断方式等。

--- src/Zxc.Python/zxcPy.Robot/zxcPy.Robot/Prjs/myRobot_StockAppetite.py
+++ src/Zxc.Python/zxcPy.Robot/zxcPy.Robot/Prjs/myRobot_StockAppetite.py
@@ -73,8 +73,9 @@ class myRobot_StockAppetite(myRobot.myRobot):
         #按文字内容组合定义图片类型
         num = txtInfo['wordText'].count('资产分析')
         if(num >= 1):
-            if(myData.Matching_strs(txtInfo['wordText'], {"今年": 2, "近半年": 1, "本月": 1, "跑嬴": 1})):
+            if(myData.Matching_strs(txtInfo['wordText'], {"今年": 2, "近半年": 1, "本月": 1, "近三": 1})):
                 if(num == 2):       #君弘截图
+                    #'10:09:1l\n(80\n资产分析\n20190101~20190828\n今年收益率17.48%\n今日本月近三个月近半年今年2018\n30%\n20%\n2019.01.31\n我:-0.11\n10%\n上证:3.64%\n10%\n01.31\n03.29\n05.31\n07.31\n●我○上证深证○创业\n1.46%\n跑赢上证\n1.46%\n跑输深证\n-12.56%\n跑输创业\n-12.47%\n资产分析\n证券分析\n月度分析\n账单'
                     #'10:09i\n@(80\n资产分析\n20190101~20190828\n今年收益率17.48%\n今日本月近三个月近半年今年2018\n30%\n20%\n2019.01.31\n我:-0.11%\n10%\n上证:3.64%\n10%\n01.31\n03.29\n05.31\n07.31\n我\n上证\n深证\n○创业\n1.46%\n跑嬴上证\n1.46%\n跑输深证\n-12.56%\n跑输创业\n-12.47%\n资产分析\n证券分析\n月度分析\n账单'
                     imageInfo = myAI_Baidu.IORC(Text, "18ed022a1b51ef96b130bd4226b89f64", out_debug=False)
                     dictInfos['收益率'] = imageInfo['收益率']['word'].replace('今年收益率', "").replace('%', "")
@@ -82,18 +83,19 @@ class myRobot_StockAppetite(myRobot.myRobot):
                     if(times[0][0:4] == times[1][0:4]):
                         dictInfos['日期'] = myData_Trans.Tran_ToDatetime(times[1], '%Y%m%d')
                 elif(num == 1 and txtInfo['wordText'].count('同花顺-资产分析') == 1):     #同花顺截图
-                    #'u中国联通令\n下午3:10\n64%\n同花顺-资产分析\n2019-01-01~2019-08-27\n分享\n本月\n近三月\n近半年\n今年\n近两年\n30.0%\n10.0%\n0.0%\n-10.0%\n01-01\n04-01\n07-01\n我上证○深证○创业板○上证50\nC沪深30中证500收益走势\n今年跑嬴上证指数\n23.03%\n我\n3940%\n上证\n16.37%\n深证\n30.43%\n创业板\n30.19%'
+                    #'u中国联通令\n下午3:10\n64%\n同花顺-资产分析\n2019-01-01~2019-08-27\n分享\n本月\n近三月\n近半年\n今年\n近两年\n30.0%\n10.0%\n0.0%\n-10.0%\n01-01\n04-01\n07-01\n我上证○深证○创业板○上证50\nC沪深30中证500收益走势\n今年跑嬴上证指数\n23.03%\n我\n3940%\n上证\n16.37%\n深证\n30.43%\n创业板\n30.19%' 
                     imageInfo = myAI_Baidu.IORC(Text, "c0f24215c88dcee9c9f5111238b31c96", out_debug=False)
                     dictInfos['收益率'] = imageInfo['收益率']['word'].replace('%', "")
                     times = imageInfo['时间段']['word'].split('~')
                     if(times[0][0:4] == times[1][0:4]):
                         dictInfos['日期'] = myData_Trans.Tran_ToDatetime(times[1], '%Y-%m-%d')
 
-        # 修正日期为字符串
-        if(dictInfos.get('日期', "") != ""):
-            dictInfos['日期'] = myData_Trans.Tran_ToDatetime_str(dictInfos['日期'], '%Y-%m-%d')
-        usrName = usrInfo.get('usrNameNick', "") 
-        return "@" + usrName + " " + str(dictInfos) 
+            # 修正日期为字符串
+            if(dictInfos.get('日期', "") != ""):
+                dictInfos['日期'] = myData_Trans.Tran_ToDatetime_str(dictInfos['日期'], '%Y-%m-%d')
+            usrName = usrInfo.get('usrNameNick', "") 
+            return "@" + usrName + " " + str(dictInfos) 
+        return ""
     
     def _Title_User_Opened(self): 
         return "自动处理所有股票偏好消息..."
@@ -124,7 +126,7 @@ if __name__ == "__main__":
 
 
     #图片自动处理
-    myDebug.Debug(pRobot_Stock.Done("E:\\myCode\\zxcProj\\src\\Zxc.Python\\zxcPy.All.Base\\Temps\\Images\\Test2.jpg", msgType = "PICTURE", usrNameNick='茶叶一主号',)['msg'])  
+    myDebug.Debug(pRobot_Stock.Done("E:\\myCode\\zxcProj\\src\\Zxc.Python\\zxcPy.All.Base\\Temps\\Images\\Test3.jpg", msgType = "PICTURE", usrNameNick='茶叶一主号',)['msg'])  
     myDebug.Debug(pRobot_Stock.Done("E:\\myCode\\zxcProj\\src\\Zxc.Python\\zxcPy.All.Base\\Temps\\Images\\Test.png", msgType = "PICTURE", usrNameNick='茶叶一主号',)['msg'])  
 
 
