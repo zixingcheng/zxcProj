bf06c3ac3ead512cd02f21f1b40cacc55a7db1fd
墨紫 <164750211@qq.com>
2019-09-16 23:18:55 +0800

    测试--修正股票参数设置同步不一致问题。

--- src/Zxc.Python/zxcPy.Quote/zxcPy.APIs/myAPI_Quote.py
+++ src/Zxc.Python/zxcPy.Quote/zxcPy.APIs/myAPI_Quote.py
@@ -51,8 +51,8 @@ class myAPI_Quote_Set(myWeb.myAPI):
                     editInfo = myData_Trans.Tran_ToDict(request.args.get('editInfo', "{}"))
                     
                     # 特殊同步
-                    usrIDs = editInfo.get("msgUsers",{})
-                    if(usrIDs.get("茶叶一主号","") != "" or usrIDs.get("老婆","") != ""):
+                    usrIDs = editInfo.get("msgUsers",{}).keys()
+                    if("茶叶一主号" in usrIDs or "老婆" in usrIDs):
                         usrPlat = request.args.get('usrPlat', 'wx')
                         editInfo['msgUsers']['茶叶一主号'] = usrPlat
                         editInfo['msgUsers']['老婆'] = usrPlat
