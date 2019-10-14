d36e5c9176a8f4f4eb90ff9326acb4abb0d00d23
zxc <zxc@163.com>
2019-10-14 20:57:54 +0800

    修改--增加聚宽行情价格、周期价格信息等；

--- src/Zxc.Python/zxcPy.Quote/zxcPy.Quotation/Quote_Source/mySource_JQData_API.py
+++ src/Zxc.Python/zxcPy.Quote/zxcPy.Quotation/Quote_Source/mySource_JQData_API.py
@@ -13,7 +13,7 @@ jqdatasdk.auth('18002273029','zxcvbnm.123')     # 账户登陆
 
 #引用根目录类文件夹--必须，否则非本地目录起动时无法找到自定义类 
 mySystem.Append_Us("", False)    
-import myData_Trans, myDebug
+import myData_Trans, myData, myDebug
 from myGlobal import gol   
 from jqdatasdk import *
 
@@ -29,13 +29,13 @@ class Source_JQData_API():
         # 未实现
         return True 
 
-    #生成数据对象
+    #生成数据对象--未完成
     def newData(self):
         return myData_Stock.Data_Stock()
+    #生成数据对象--未完成
     def newData_ByInfo(self, dataInfo, checkTime = True):     
         return None
-
-    #生成数据集对象
+    #生成数据集对象--未完成
     def newDatas(self, data, interval):
         return myData_Stock.Datas_Stock(data, interval)
 
@@ -52,11 +52,51 @@ class Source_JQData_API():
     # 获取概念板块成分股
     def getConcepts_Stocks(self, concept_code, data=None):
         return get_concept_stocks(concept_code, date=None)
+    
+
+    # 查询期权信息，指定日期，默认当天
+    def getOptInfo(self, optPrice = 2500, dateTime = ""):
+        # 同步参数
+        if(dateTime == ""): dateTime = myData_Trans.Tran_ToDatetime_str(None, "%Y-%m-%d")
+        dtFliter = myData_Trans.Tran_ToDatetime(dateTime, "%Y-%m-%d")
+        strFliter = "50ETF" + myData.iif(optPrice > 0, "购", "沽") + str(dtFliter.month) + "月" + str(optPrice)
+
+        # 查询
+        optInfos = opt.run_query(query(opt.OPT_DAILY_PREOPEN).filter(opt.OPT_DAILY_PREOPEN.date==dateTime,opt.OPT_DAILY_PREOPEN.name==strFliter).limit(10))
+        if(optInfos.shape[0] != 1): return None
+        
+        # 查询最新的期权基本资料数据
+        code = optInfos['code'][0]
+        optInfo = query(opt.OPT_CONTRACT_INFO).filter(opt.OPT_CONTRACT_INFO.code==code)
+        value = opt.run_query(optInfo)
+        if(len(value) != 1): return None
+        return {'id': value['id'][0], 'code': value['code'][0], 'trading_code': value['trading_code'][0]}
+
+    # 查询价格信息，指定日期范围、数据频率
+    # fields:['open', ' close', 'low', 'high', 'volume', 'money', 'factor', 'high_limit',' low_limit', 'avg', ' pre_close', 'paused']
+    def getPrice(self, security, start_date="", end_date="", frequency='daily', fields=None, skip_paused=True, count=None):
+        # 同步参数
+        dateTime = myData_Trans.Tran_ToDatetime_str(None, "%Y-%m-%d")
+        start_date = myData.iif(start_date == "", dateTime + " 09:00:00", start_date)
+        end_date = myData.iif(end_date == "", dateTime + " 13:00:00", end_date)
+        
+        # 查询数据
+        values = jqdatasdk.get_price(security=security, frequency=frequency, start_date=start_date, end_date=end_date, skip_paused=skip_paused) 
+        return values
+
+    # 查询周期价格信息，'1m', '5m', '15m', '30m', '60m', '120m', '1d', '1w', '1M'
+    # fields:['date', 'open', 'close', 'high', 'low', 'volume', 'money']
+    def getPrice_bars(self, security, count, end_date="", unit='1d', include_now=False, fields=None):
+        # 查询数据
+        if(fields == None): fields=['date','open','high','low','close']
+        values = jqdatasdk.get_bars(security=security, count=count, unit=unit, fields=fields, include_now=include_now, end_dt=end_date) 
+        return values
 
 
+    
 #缓存全局对象
 gol._Set_Value('quoteSource_API', Source_JQData_API())  #实例 行情对象
-myDebug.Debug(jqdatasdk.get_query_count())
+myDebug.Debug(jqdatasdk.get_query_count())              #打印当日可请求条数
 
 
 
@@ -66,6 +106,14 @@ if __name__ == "__main__":
     pSource = gol._Get_Value('quoteSource_API', None)
     print(pSource.getSecurities())
     print(pSource.getSecurities('index'))
- 
+
+    # 提取50期权信息
+    print("当天3000的期权信息：")
+    opt = pSource.getOptInfo(3000)
+    print(opt)
+    print(pSource.getPrice(security=opt['code'],frequency='1m',start_date='2019-10-14 09:00:00',end_date='2019-10-14 15:00:00'))
+    print(pSource.getPrice_bars(security=opt['code'],count=1,unit='1d',end_date='2019-10-14 15:00:00',include_now=False))
+    print(pSource.getPrice_bars(security=opt['code'],count=1,unit='1d',end_date='2019-10-14 15:00:00',include_now=True))
+     
     
     print()
