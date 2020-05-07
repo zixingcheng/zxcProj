c9240c31fc3655a5d550527a1e7a0753818430b6
zxc <zxc@zxc.com>
2020-05-07 11:38:39 +0800

    修改--调整Quote输出内容到文件，便于调试；

--- ReadMe/run_zxcPy_Quote.sh
+++ ReadMe/run_zxcPy_Quote.sh
@@ -11,7 +11,7 @@ file_Quote=$dirPath_srcPy/zxcPy.Quote/myQuote_API.py
 
 count_Quote=`ps -ef |grep $file_Quote |grep -v "grep" |wc -l`
 if [ 0 == $count_Quote ];then
-	nohup python $file_Quote  >  /dev/null 2>&1 &
+	nohup python $file_Quote > $logfile_Wx 2>&1 &
 echo $count_Quote
 fi
 
