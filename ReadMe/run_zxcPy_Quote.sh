e5fc18bc50dea2a071f3537a4bfda44707628b98
zxc <zxc@zxc.com>
2020-05-12 18:45:18 +0800

    修改--调整Linux执行脚本，增加文件方式控制调试模式；

--- ReadMe/run_zxcPy_Quote.sh
+++ ReadMe/run_zxcPy_Quote.sh
@@ -4,16 +4,21 @@
 dirPath=/root/Public/myPrjs/zxcProj
 dirPath_srcPy=/root/Public/myPrjs/zxcProj/src/Zxc.Python
 
+#调试判断-存在文件中断(用于调试)
+file_Debug=$dirPath/ReadMe/zxcPy_Debug.log 
+if [ -d "$file_Debug"]; then  
+　　exit
+fi
+
 
 #启动行情监测
-#logfile_Quote=$dirPath/Logs/myQuote_API.log
-#file_Quote=$dirPath_srcPy/zxcPy.Quote/myQuote_API.py
+logfile_Quote=$dirPath/Logs/myQuote_API.log
+file_Quote=$dirPath_srcPy/zxcPy.Quote/myQuote_API.py
 
-#count_Quote=`ps -ef |grep $file_Quote |grep -v "grep" |wc -l`
-#if [ 0 == $count_Quote ];then
-#	#nohup python $file_Quote  >  /dev/null 2>&1 &
-#	nohup python $file_Quote > $logfile_Quote 2>&1 &
-#echo $count_Quote
-#fi
-
+count_Quote=`ps -ef |grep $file_Quote |grep -v "grep" |wc -l`
+if [ 0 == $count_Quote ];then
+	#nohup python $file_Quote  >  /dev/null 2>&1 &
+	nohup python $file_Quote > $logfile_Quote 2>&1 &
+echo $count_Quote
+fi
 
--- ReadMe/run_zxcPy_Robot.sh
+++ ReadMe/run_zxcPy_Robot.sh
@@ -4,27 +4,32 @@
 dirPath=/root/Public/myPrjs/zxcProj
 dirPath_srcPy=/root/Public/myPrjs/zxcProj/src/Zxc.Python
 
+#调试判断-存在文件中断(用于调试)
+file_Debug=$dirPath/ReadMe/zxcPy_Debug.log 
+if [ -d "$file_Debug"]; then  
+　　exit
+fi
+
 
 #启动微信后台
-#logfile_Wx=$dirPath/Logs/myWeixin_ItChat.log
-#file_Wx=$dirPath_srcPy/zxcPy.Weixin/zxcPy.Weixin/myWeixin_ItChat.py
+logfile_Wx=$dirPath/Logs/myWeixin_ItChat.log
+file_Wx=$dirPath_srcPy/zxcPy.Weixin/zxcPy.Weixin/myWeixin_ItChat.py
 
-#count_Wx=`ps -ef |grep $file_Wx |grep -v "grep" |wc -l`
-#if [ 0 == $count_Wx ];then
-#	nohup python $file_Wx  > $logfile_Wx 2>&1 &
-#echo $count_Wx
-#fi
-#sleep 10s
+count_Wx=`ps -ef |grep $file_Wx |grep -v "grep" |wc -l`
+if [ 0 == $count_Wx ];then
+	nohup python $file_Wx  > $logfile_Wx 2>&1 &
+echo $count_Wx
+fi
+sleep 10s
 
 
 #启动微信API
-#logfile_API=$dirPath/Logs/myRobot_API.log
-#file_API=$dirPath_srcPy/zxcPy.Robot/myRobot_API.py
+logfile_API=$dirPath/Logs/myRobot_API.log
+file_API=$dirPath_srcPy/zxcPy.Robot/myRobot_API.py
 
-#count_API=`ps -ef |grep $file_API |grep -v "grep" |wc -l`
-#if [ 0 == $count_API ];then
-#	nohup python $file_API  > $logfile_API 2>&1 &
-#echo $count_API
-#fi
-
+count_API=`ps -ef |grep $file_API |grep -v "grep" |wc -l`
+if [ 0 == $count_API ];then
+	nohup python $file_API  > $logfile_API 2>&1 &
+echo $count_API
+fi
 
